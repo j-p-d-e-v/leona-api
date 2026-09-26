2037b67ca8d4a2372899e40a9343c5cdbcf01e78
JP Laptop <26633918+jpmateo022@users.noreply.github.com>
2026-09-26 14:41:29 +0800

    target weight inprogress

--- crates/leona-health/src/weight/weight_target.rs
+++ crates/leona-health/src/weight/weight_target.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{result, sync::Arc};
 
 use chrono::{DateTime, Utc};
 use leona_core::Error;
@@ -8,33 +8,34 @@ use sqlx::{AssertSqlSafe, prelude::FromRow};
 use crate::weight::WeightUnit;
 
 #[derive(Debug, Clone)]
-pub struct WeightEntries {
+pub struct TargetWeight {
     db_client: Arc<Db>,
 }
 
 #[derive(Debug, Clone, FromRow)]
-pub struct WeightEntryData {
+pub struct TargetWeightData {
     id: Option<i64>,
+    user_id: i64,
     weight: f32,
     unit: WeightUnit,
     notes: Option<String>,
-    created_at: DateTime<Utc>,
+    created_at: Option<DateTime<Utc>>,
     updated_at: Option<DateTime<Utc>>,
 }
 
-impl WeightEntries {
+impl TargetWeight {
     pub async fn new(db_client: Arc<Db>) -> Result<Self, Error> {
         Self::migrate(db_client.clone()).await?;
         Ok(Self { db_client })
     }
 
     pub fn table() -> String {
-        "leona_weight_entries".to_string()
+        "leona_target_weight".to_string()
     }
 
     /*
      * Migrate
-     * Put your sql queries that modifies the weight entries schema.
+     * Put your sql queries that modifies the target weight schema.
      * Make sure that is in sequence.
      */
     async fn migrate(db_client: Arc<Db>) -> Result<(), Error> {
@@ -49,6 +50,7 @@ impl WeightEntries {
                 r#"
                    CREATE TABLE IF NOT EXISTS {} (
                        id            INTEGER PRIMARY KEY AUTOINCREMENT,
+                       user_id       INTEGER NOT NULL,
                        weight        FLOAT NOT NULL,
                        unit          TEXT NOT NULL,
                        notes         TEXT NULL,
@@ -69,30 +71,14 @@ impl WeightEntries {
         }
     }
 
-    pub async fn list(&self) -> Result<Vec<WeightEntryData>, Error> {
+    pub async fn get(&self, user_id: i64) -> Result<TargetWeightData, Error> {
         {
             let mut connection = self.db_client.get_connection().await?;
 
-            let query_stmt = format!("SELECT * FROM {}", Self::table());
+            let query_stmt = format!("SELECT * FROM {} WHERE user_id = $1", Self::table());
 
-            match sqlx::query_as::<_, WeightEntryData>(AssertSqlSafe(query_stmt))
-                .fetch_all(&mut *connection)
-                .await
-            {
-                Ok(result) => Ok(result),
-                Err(error) => Err(Error::DbQueryErr(error.to_string())),
-            }
-        }
-    }
-
-    pub async fn get(&self, id: i64) -> Result<WeightEntryData, Error> {
-        {
-            let mut connection = self.db_client.get_connection().await?;
-
-            let query_stmt = format!("SELECT * FROM {} WHERE id = $1", Self::table());
-
-            match sqlx::query_as::<_, WeightEntryData>(AssertSqlSafe(query_stmt))
-                .bind(id)
+            match sqlx::query_as::<_, TargetWeightData>(AssertSqlSafe(query_stmt))
+                .bind(user_id)
                 .fetch_one(&mut *connection)
                 .await
             {
@@ -102,25 +88,55 @@ impl WeightEntries {
         }
     }
 
-    pub async fn create(&self, data: WeightEntryData) -> Result<(), Error> {
+    pub async fn is_exists(&self, user_id: i64) -> Result<bool, Error> {
+        {
+            let mut connection = self.db_client.get_connection().await?;
+
+            let query_stmt = format!("SELECT 1 FROM {} WHERE user_id = $1", Self::table());
+
+            match sqlx::query(AssertSqlSafe(query_stmt))
+                .bind(user_id)
+                .fetch_optional(&mut *connection)
+                .await
+            {
+                Ok(result) => Ok(result.is_some()),
+                Err(error) => Err(Error::DbQueryErr(error.to_string())),
+            }
+        }
+    }
+
+    pub async fn save(&self, data: TargetWeightData) -> Result<TargetWeightData, Error> {
+        let user_id = data.user_id.clone();
+        let is_exists = self.is_exists(user_id).await?;
+        if is_exists {
+            let _ = self.create(data).await?;
+        } else {
+            let _ = self.update(data).await?;
+        };
+        Ok(self.get(user_id).await?)
+    }
+
+    pub async fn create(&self, data: TargetWeightData) -> Result<(), Error> {
         {
             let mut connection = self.db_client.get_connection().await?;
 
             let query_stmt = format!(
                 "INSERT INTO {}(
+                    user_id,
                     weight,
                     unit,
-                    notes
+                    notes,
                     created_at
                 ) VALUES(?,?,?,?)",
                 Self::table()
             );
 
             match sqlx::query(AssertSqlSafe(query_stmt))
+                .bind(data.user_id)
                 .bind(data.weight)
                 .bind(data.unit)
                 .bind(data.notes)
-                .bind(data.created_at)
+                .bind(Utc::now())
                 .execute(&mut *connection)
                 .await
             {
@@ -130,12 +146,12 @@ impl WeightEntries {
         }
     }
 
-    pub async fn update(&self, data: WeightEntryData) -> Result<(), Error> {
+    pub async fn update(&self, data: TargetWeightData) -> Result<bool, Error> {
         {
             let mut connection = self.db_client.get_connection().await?;
 
             let query_stmt = format!(
-                "UPDATE {} SET weight = ?, unit = ?, notes = ?, updated_at = ? WHERE id = ?",
+                "UPDATE {} SET weight = ?, unit = ?, notes = ?, updated_at = ? WHERE user_id = ?",
                 Self::table()
             );
 
@@ -143,25 +159,25 @@ impl WeightEntries {
                 .bind(data.weight)
                 .bind(data.unit)
                 .bind(data.notes)
-                .bind(data.updated_at)
-                .bind(data.id)
+                .bind(Utc::now())
+                .bind(data.user_id)
                 .execute(&mut *connection)
                 .await
             {
-                Ok(_) => Ok(()),
+                Ok(result) => Ok(result.rows_affected() > 0),
                 Err(error) => Err(Error::DbQueryErr(error.to_string())),
             }
         }
     }
 
-    pub async fn delete(&self, id: i64) -> Result<bool, Error> {
+    pub async fn delete(&self, user_id: i64) -> Result<bool, Error> {
         {
             let mut connection = self.db_client.get_connection().await?;
 
-            let query_stmt = format!("DELETE FROM {} WHERE id = $1", Self::table());
+            let query_stmt = format!("DELETE FROM {} WHERE user_id = $1", Self::table());
 
             match sqlx::query(AssertSqlSafe(query_stmt))
-                .bind(id)
+                .bind(user_id)
                 .execute(&mut *connection)
                 .await
             {
